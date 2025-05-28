743caadd1aaa1ab2703e2fec0b88e99bea2e419c
Alexandr Neverov <nev.al.vic1@gmail.com>
2025-05-27 20:21:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,5 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip && \
 sudo ./aws/install && \
 aws --version && \
-terraform -v'
+terraform -v && \
+echo "Congratulations! Terraform and AWS CLI have been installed!"'
